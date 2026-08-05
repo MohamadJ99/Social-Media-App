244e8ad834c52411269775241bd828cd6acd61f3
MohamadJ99 <mohamadjawad9991@gmail.com>
2026-08-05 23:40:59 +0300

    feat: add second screen and Clerk authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { ClerkProvider} from '@clerk/nextjs'
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
@@ -38,5 +41,6 @@ export default function RootLayout({
       
         </body>
     </html>
+    </ClerkProvider>
   );
 }
