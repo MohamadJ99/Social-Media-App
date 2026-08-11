1dd5bfd400c177ee58bea4df5a1e5070d64fc7cb
MohamadJ99 <mohamadjawad9991@gmail.com>
2026-08-11 07:10:23 +0300

    style: improve post layout

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const AddPost = () => {
   return (
-    <div className="p-4 bg-white rounded-lg flex gap-4 justify-between text-sm">
+    <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
   {/* AVATAR */}
   <Image src="https://images.pexels.com/photos/29883936/pexels-photo-29883936.jpeg" alt="" width={48} height={48} className="w-12 h-12 object-cover rounded-full"/>
  
@@ -19,7 +19,7 @@ const AddPost = () => {
            <Image src="/addimage.png" alt="" width={20} height={20} />
            Photo
         </div>
-        
+
         <div className="flex items-center gap-2 cursor-pointer">
            <Image src="/addVideo.png" alt="" width={20} height={20} />
            Video
---
+++ src/components/Post.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+
+const Post = () => {
+  return (
+    <div className="flex flex-col gap-4">
+    
+    {/* USER */}
+    <div className="flex items-center justify-between">
+     <div className="flex items-center gap-4">
+     <Image src="https://images.pexels.com/photos/1311311/pexels-photo-1311311.jpeg" alt="" width={40} height={40} className="w-10 h-10 rounded-full"/>
+     <span className="font-medium">Nasser Jehad</span>
+     </div>
+     <Image src="/more.png" alt="" width={16} height={16} />
+    </div>
+
+    {/* DESC */}
+    <div className="flex flex-col gap-4">
+     <div className="w-full min-h-96 relative">
+      <Image src="https://images.pexels.com/photos/19900476/pexels-photo-19900476.jpeg" alt="" fill className="object-cover rounded-md"/>
+     </div>
+     <p>
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+         Excepteur sint occaecat cupidatat non proident,
+         sunt in culpa qui officia deserunt mollit anim id est laborum.
+     </p>
+    </div>
+
+    {/* INTERACTION */}
+    <div className="flex items-center justify-between text-sm my-4">
+     <div className="flex gap-8">
+        <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+            <Image src="/like.png" alt="" width={16} height={16} className="cursor-pointer"/>
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">123 <span className="hidden md:inline">Likes</span></span>
+        </div>
+
+        <div className="flex items-center gap-4 bg-slate-50 p-2 rounded-xl">
+            <Image src="/comment.png" alt="" width={16} height={16} className="cursor-pointer"/>
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">20 <span className="hidden md:inline">Comments</span></span>
+        </div>
+
+     </div>
+     <div className="">
+     <div className="flex items-center gap-4 bg-slate-100 p-2 rounded-xl">
+            <Image src="/share.png" alt="" width={16} height={16} className="cursor-pointer"/>
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">4 <span className="hidden md:inline">Shares</span></span>
+        </div>
+
+     </div>
+    </div>
+
+
+    </div>
+  )
+}
+
+export default Post;
