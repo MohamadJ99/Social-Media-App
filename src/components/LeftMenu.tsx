f43eaf3c786e8b31de4b46bf7c0ed6ac202adc39
MohamadJ99 <mohamadjawad9991@gmail.com>
2026-08-16 22:15:56 +0300

    refactor: update left menu layout

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,8 +1,87 @@
+import Link from "next/link";
+import ProfileCard from "./ProfileCard";
+import Image from "next/image";
+import Ad from "./Ad";
 
-const LeftMenu = () => {
+
+const menuItems = [
+  {
+    name: "My Posts",
+    href: "/",
+    icon: "/posts.png",
+  },
+  {
+    name: "Activity",
+    href: "/",
+    icon: "/activity.png",
+  },
+  {
+    name: "Marketplace",
+    href: "/",
+    icon: "/market.png"
+  },
+  {
+    name: "Albums",
+    href: "/",
+    icon: "/albums.png"
+  },
+  {
+    name: "Videos",
+    href: "/",
+    icon: "/videos.png"
+  },
+  {
+    name: "News",
+    href: "/",
+    icon: "/news.png"
+  },
+   {
+    name: "Courses",
+    href: "/",
+    icon: "/courses.png"
+  },
+   {
+    name: "Lists",
+    href: "/",
+    icon: "/lists.png"
+  },
+  {
+    name: "Settings",
+    href: "/",
+    icon: "/settings.png"
+  }
+];
+
+
+const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
   return (
-    <div>left menu</div>
-  )
-}
+    <div className="flex flex-col gap-6">
+
+      {type === "home" && <ProfileCard />}
+
+      <div className="flex flex-col gap-2 p-4 bg-white rounded-lg shadow-md text-sm text-gray-500">
+
+        {menuItems.map((item) => (
+          <Link
+            key={item.name}
+            href={item.href}
+            className="flex items-center gap-4 p-2 rounded-lg hover:bg-slate-100"
+          >
+            <Image
+              src={item.icon}
+              alt=""
+              width={20}
+              height={20}
+            />
+
+            <span>{item.name}</span>
+          </Link>
+        ))}
+
+      </div>
+      <Ad size="sm"/>
+    </div>
+  );
+};
 
 export default LeftMenu;
